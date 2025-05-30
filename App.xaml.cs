199a19439a8f16608ee50543825301841df4642b
Beatriz Prado <beatrizgprado.bp@gmail.com>
2025-05-30 20:58:07 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto ()
+            {
+                Descricao = "Suite Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+                },
+new Quarto ()
+            {
+                Descricao = "Suite Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+                },
+new Quarto ()
+            {
+                Descricao = "Suite Basic",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+                },
+new Quarto ()
+            {
+                Descricao = "Suite Crise",
+                ValorDiariaAdulto = 25.0,
+                ValorDiariaCrianca = 12.0
+                },
+
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContrataçaoHospedagem());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("LavishlyYours-Regular.ttf", "LavishlyYours-Regular");
+                    fonts.AddFont("MysteryQuest-Regular.ttf", "MysteryQuest-Regular");
+                    fonts.AddFont("PoiretOne-Regular.ttf", "PoiretOne-Regular");
+
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
